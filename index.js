e0f9dc0b8b775fa66434c2485735443ad3a6c5ab
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2025-10-22 15:53:46 +0000

    todos

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+let todos = ['first todo', 'second todo', 'third todo'];
+let i = 0;
 
+function markAsDone(todos){
+    while(i < todos.length){
+        i++
+        todos[i] = 'done - ' + todos[i];
+    }
+    return todos;
+}
+
+document.getElementById('result').innerHTML = todos;
